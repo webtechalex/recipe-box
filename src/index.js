c69052edad86636b7bf3df71f4cd942cdb3bd46e
Alex Brown <alexanderbrown.info@gmail.com>
2017-11-07 16:19:35 +0000

    render first component

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import App from './components/App';
+
+// import global styles here...
+
+ReactDOM.render(<App />, document.getElementById('app'));
