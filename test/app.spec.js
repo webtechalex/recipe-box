03cb8996b4d54b038d256c970575618264dcd991
Alex Brown <alexanderbrown.info@gmail.com>
2017-11-08 13:31:28 +0000

    added tests for initial state

--- test/app.spec.js
+++ test/app.spec.js
@@ -1,7 +1,22 @@
 import {expect} from 'chai';
 
-describe('Should run a test', function() {
+import recipesReducer from '../src/reducers/recipes';
+
+describe('Test environment', function() {
   it('should run a test', function() {
     expect(true).to.equal(true);
+  });
+});
+
+describe('Application state', function() {
+  it('should return the intial state when no action is givem', function() {
+    expect(recipesReducer()).to.eql([]);
+    expect(recipesReducer(['pizza'])).to.eql(['pizza']);
+  });
+  it('should return the previous state given an unrecognised action', function() {
+    expect(recipesReducer([], 'BBBLLLLLLLLLAAAAAAAAAAAHHH')).to.eql([]);
+  })
+  xit('should add a recipe to the application state with action: ADD_RECIPE', function() {
+
   });
 });
