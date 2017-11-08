f7f9f99d0499f33c6d96ed9dc004a4426902f74b
Alex Brown <alexanderbrown.info@gmail.com>
2017-11-08 18:08:35 +0000

    configured provider component

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,17 @@
 import React from 'react';
+import {Provider} from 'react-redux';
+import {createStore} from 'redux';
 
+import recipesReducer from '../reducers/recipes';
 import Container from './Container';
+
+const store = createStore(recipesReducer);
 
 const App = () => {
   return (
-    <Container />
+    <Provider store={store}>
+      <Container />
+    </Provider>
   );
 }
 
